b1422347d637dfec9394932e92e1e731d283b29b
ehsanghasempouri <97990549+ehsanghasempouri@users.noreply.github.com>
2023-05-05 15:20:43 -0300

    Add files via upload

---
+++ H4, reps=1,2,3,  0.6-0.8, paper.py
@@ -0,0 +1,632 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from qiskit import*
+from qiskit.circuit import Parameter
+import numpy as np
+from qiskit import Aer
+from qiskit_nature.drivers import UnitsType, Molecule
+from qiskit_nature.drivers.second_quantization import (
+    ElectronicStructureDriverType,
+    ElectronicStructureMoleculeDriver,
+)
+from qiskit_nature.problems.second_quantization import ElectronicStructureProblem
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper, ParityMapper, BravyiKitaevMapper
+
+
+enrgy2=[]
+par2=[]
+spin2=[]
+b=0.735
+for a in np.arange(0.4,2.1,0.1, dtype=object):
+    molecule = Molecule(
+        geometry=[["H", [0.0, 0.0, 0.0]], ["H", [a,0.0, 0]], ["H", [0.0, b, 0]],["H", [a, b, 0.0]]], charge=0, multiplicity=1
+    )
+    driver = ElectronicStructureMoleculeDriver(
+        molecule, basis="sto3g", driver_type=ElectronicStructureDriverType.PYQUANTE
+    )
+
+    es_problem = ElectronicStructureProblem(driver)
+    qubit_converter = QubitConverter(ParityMapper(),two_qubit_reduction=True)
+    from qiskit.providers.aer import StatevectorSimulator
+    
+    from qiskit import Aer
+    from qiskit.utils import QuantumInstance,algorithm_globals
+    from qiskit_nature.algorithms import VQEUCCFactory
+
+    quantum_instance = QuantumInstance(backend=Aer.get_backend("aer_simulator_statevector"))
+    vqe_solver = VQEUCCFactory(quantum_instance)
+
+    from qiskit.algorithms import VQE
+    from qiskit.circuit.library import TwoLocal
+
+    seed = 50
+    algorithm_globals.random_seed = seed
+    
+    num_qubits = 6
+
+    
+    layer_1 = [(0, 5),(1,4),(2,3)]
+    layer_2 = [(0, 3),(1,2),(4,5)]
+    
+    
+    ansatz=TwoLocal(num_qubits, 'ry', 'cx',[layer_1,layer_2]
+                      , reps=2,insert_barriers=True, parameter_prefix = 'theta')
+    
+  
+    
+    
+    
+    from qiskit.algorithms.optimizers import SLSQP
+    slsqp = SLSQP(maxiter=200)
+    another_solver = VQE(
+        ansatz,optimizer=slsqp,
+        quantum_instance=QuantumInstance(Aer.get_backend('statevector_simulator'), seed_transpiler=seed, seed_simulator=seed),
+    )
+    from qiskit_nature.algorithms import GroundStateEigensolver
+
+    calc = GroundStateEigensolver(qubit_converter, another_solver)
+    res = calc.solve(es_problem)
+
+    print("a:",a)
+    print("")
+    print(res)
+    print("")
+    print("Total ground state energy=",res.total_energies)
+    print("")
+    #print("absolute tolerance= ",  res.ABSOLUTE_TOLERANCE )
+    print("=======================***************============================")
+    enrgy2.append(res.total_energies[0].real)
+    par2.append(res.num_particles)
+    spin2.append(res.spin)
+    
+      
+    
+
+
+
+
+ansatz.decompose().draw(output = 'mpl')
+
+
+# In[2]:
+
+
+print("energy= ", enrgy2)
+print("")
+print("=======================***************============================")
+print("particle number"  ,par2)
+print("")
+print("=======================***************============================")
+print("spin"  ,spin2)
+
+
+# In[3]:
+
+
+from qiskit import*
+from qiskit.circuit import Parameter
+import numpy as np
+from qiskit import Aer
+from qiskit_nature.drivers import UnitsType, Molecule
+from qiskit_nature.drivers.second_quantization import (
+    ElectronicStructureDriverType,
+    ElectronicStructureMoleculeDriver,
+)
+from qiskit_nature.problems.second_quantization import ElectronicStructureProblem
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper, ParityMapper, BravyiKitaevMapper
+
+
+enrgy4=[]
+par4=[]
+spin4=[]
+b=0.735
+for a in np.arange(0.4,2.1,0.1, dtype=object):
+    molecule = Molecule(
+        geometry=[["H", [0.0, 0.0, 0.0]], ["H", [a,0.0, 0]], ["H", [0.0, b, 0]],["H", [a, b, 0.0]]], charge=0, multiplicity=1
+    )
+    driver = ElectronicStructureMoleculeDriver(
+        molecule, basis="sto3g", driver_type=ElectronicStructureDriverType.PYQUANTE
+    )
+
+    es_problem = ElectronicStructureProblem(driver)
+    qubit_converter = QubitConverter(ParityMapper(),two_qubit_reduction=True)
+    from qiskit.providers.aer import StatevectorSimulator
+    
+    from qiskit import Aer
+    from qiskit.utils import QuantumInstance,algorithm_globals
+    from qiskit_nature.algorithms import VQEUCCFactory
+
+    quantum_instance = QuantumInstance(backend=Aer.get_backend("aer_simulator_statevector"))
+    vqe_solver = VQEUCCFactory(quantum_instance)
+
+    from qiskit.algorithms import VQE
+    from qiskit.circuit.library import TwoLocal
+
+    seed = 50
+    algorithm_globals.random_seed = seed
+    
+    num_qubits = 6
+
+    
+    layer_1 = [(0, 5),(1,4),(2,3)]
+    layer_2 = [(0, 3),(1,2),(4,5)]
+    
+    
+    ansatz=TwoLocal(num_qubits, 'ry', 'cx',[layer_1,layer_2]
+                      , reps=4,insert_barriers=True, parameter_prefix = 'theta')
+    
+  
+    
+    
+    
+    from qiskit.algorithms.optimizers import SLSQP
+    slsqp = SLSQP(maxiter=200)
+    another_solver = VQE(
+        ansatz,optimizer=slsqp,
+        quantum_instance=QuantumInstance(Aer.get_backend('statevector_simulator'), seed_transpiler=seed, seed_simulator=seed),
+    )
+    from qiskit_nature.algorithms import GroundStateEigensolver
+
+    calc = GroundStateEigensolver(qubit_converter, another_solver)
+    res = calc.solve(es_problem)
+
+    print("a:",a)
+    print("")
+    print(res)
+    print("")
+    print("Total ground state energy=",res.total_energies)
+    print("")
+    #print("absolute tolerance= ",  res.ABSOLUTE_TOLERANCE )
+    print("=======================***************============================")
+    enrgy4.append(res.total_energies[0].real)
+    par4.append(res.num_particles)
+    spin4.append(res.spin)
+    
+      
+    
+
+
+
+
+ansatz.decompose().draw(output = 'mpl')
+
+
+# In[4]:
+
+
+print("energy= ", enrgy4)
+print("")
+print("=======================***************============================")
+print("particle number"  ,par4)
+print("")
+print("=======================***************============================")
+print("spin"  ,spin4)
+
+
+# In[5]:
+
+
+from qiskit import*
+from qiskit.circuit import Parameter
+import numpy as np
+from qiskit import Aer
+from qiskit_nature.drivers import UnitsType, Molecule
+from qiskit_nature.drivers.second_quantization import (
+    ElectronicStructureDriverType,
+    ElectronicStructureMoleculeDriver,
+)
+from qiskit_nature.problems.second_quantization import ElectronicStructureProblem
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper, ParityMapper, BravyiKitaevMapper
+
+
+enrgy2=[]
+par2=[]
+spin2=[]
+b=0.735
+for a in np.arange(0.6,0.8,0.01, dtype=object):
+    molecule = Molecule(
+        geometry=[["H", [0.0, 0.0, 0.0]], ["H", [a,0.0, 0]], ["H", [0.0, b, 0]],["H", [a, b, 0.0]]], charge=0, multiplicity=1
+    )
+    driver = ElectronicStructureMoleculeDriver(
+        molecule, basis="sto3g", driver_type=ElectronicStructureDriverType.PYQUANTE
+    )
+
+    es_problem = ElectronicStructureProblem(driver)
+    qubit_converter = QubitConverter(ParityMapper(),two_qubit_reduction=True)
+    from qiskit.providers.aer import StatevectorSimulator
+    
+    from qiskit import Aer
+    from qiskit.utils import QuantumInstance,algorithm_globals
+    from qiskit_nature.algorithms import VQEUCCFactory
+
+    quantum_instance = QuantumInstance(backend=Aer.get_backend("aer_simulator_statevector"))
+    vqe_solver = VQEUCCFactory(quantum_instance)
+
+    from qiskit.algorithms import VQE
+    from qiskit.circuit.library import TwoLocal
+
+    seed = 50
+    algorithm_globals.random_seed = seed
+    
+    num_qubits = 6
+
+    
+    layer_1 = [(0, 5),(1,4),(2,3)]
+    layer_2 = [(0, 3),(1,2),(4,5)]
+    
+    
+    ansatz=TwoLocal(num_qubits, 'ry', 'cx',[layer_1,layer_2]
+                      , reps=2,insert_barriers=True, parameter_prefix = 'theta')
+    
+  
+    
+    
+    
+    from qiskit.algorithms.optimizers import SLSQP
+    slsqp = SLSQP(maxiter=200)
+    another_solver = VQE(
+        ansatz,optimizer=slsqp,
+        quantum_instance=QuantumInstance(Aer.get_backend('statevector_simulator'), seed_transpiler=seed, seed_simulator=seed),
+    )
+    from qiskit_nature.algorithms import GroundStateEigensolver
+
+    calc = GroundStateEigensolver(qubit_converter, another_solver)
+    res = calc.solve(es_problem)
+
+    print("a:",a)
+    print("")
+    print(res)
+    print("")
+    print("Total ground state energy=",res.total_energies)
+    print("")
+    #print("absolute tolerance= ",  res.ABSOLUTE_TOLERANCE )
+    print("=======================***************============================")
+    enrgy2.append(res.total_energies[0].real)
+    par2.append(res.num_particles)
+    spin2.append(res.spin)
+    
+      
+    
+
+
+
+
+ansatz.decompose().draw(output = 'mpl')
+
+
+# In[6]:
+
+
+print("energy= ", enrgy2)
+print("")
+print("=======================***************============================")
+print("particle number"  ,par2)
+print("")
+print("=======================***************============================")
+print("spin"  ,spin2)
+
+
+# In[7]:
+
+
+from qiskit import*
+from qiskit.circuit import Parameter
+import numpy as np
+from qiskit import Aer
+from qiskit_nature.drivers import UnitsType, Molecule
+from qiskit_nature.drivers.second_quantization import (
+    ElectronicStructureDriverType,
+    ElectronicStructureMoleculeDriver,
+)
+from qiskit_nature.problems.second_quantization import ElectronicStructureProblem
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper, ParityMapper, BravyiKitaevMapper
+
+
+enrgy3=[]
+par3=[]
+spin3=[]
+b=0.735
+for a in np.arange(0.6,0.8,0.01, dtype=object):
+    molecule = Molecule(
+        geometry=[["H", [0.0, 0.0, 0.0]], ["H", [a,0.0, 0]], ["H", [0.0, b, 0]],["H", [a, b, 0.0]]], charge=0, multiplicity=1
+    )
+    driver = ElectronicStructureMoleculeDriver(
+        molecule, basis="sto3g", driver_type=ElectronicStructureDriverType.PYQUANTE
+    )
+
+    es_problem = ElectronicStructureProblem(driver)
+    qubit_converter = QubitConverter(ParityMapper(),two_qubit_reduction=True)
+    from qiskit.providers.aer import StatevectorSimulator
+    
+    from qiskit import Aer
+    from qiskit.utils import QuantumInstance,algorithm_globals
+    from qiskit_nature.algorithms import VQEUCCFactory
+
+    quantum_instance = QuantumInstance(backend=Aer.get_backend("aer_simulator_statevector"))
+    vqe_solver = VQEUCCFactory(quantum_instance)
+
+    from qiskit.algorithms import VQE
+    from qiskit.circuit.library import TwoLocal
+
+    seed = 50
+    algorithm_globals.random_seed = seed
+    
+    num_qubits = 6
+
+    
+    layer_1 = [(0, 5),(1,4),(2,3)]
+    layer_2 = [(0, 3),(1,2),(4,5)]
+    
+    
+    ansatz=TwoLocal(num_qubits, 'ry', 'cx',[layer_1,layer_2]
+                      , reps=3,insert_barriers=True, parameter_prefix = 'theta')
+    
+  
+    
+    
+    
+    from qiskit.algorithms.optimizers import SLSQP
+    slsqp = SLSQP(maxiter=200)
+    another_solver = VQE(
+        ansatz,optimizer=slsqp,
+        quantum_instance=QuantumInstance(Aer.get_backend('statevector_simulator'), seed_transpiler=seed, seed_simulator=seed),
+    )
+    from qiskit_nature.algorithms import GroundStateEigensolver
+
+    calc = GroundStateEigensolver(qubit_converter, another_solver)
+    res = calc.solve(es_problem)
+
+    print("a:",a)
+    print("")
+    print(res)
+    print("")
+    print("Total ground state energy=",res.total_energies)
+    print("")
+    #print("absolute tolerance= ",  res.ABSOLUTE_TOLERANCE )
+    print("=======================***************============================")
+    enrgy3.append(res.total_energies[0].real)
+    par3.append(res.num_particles)
+    spin3.append(res.spin)
+    
+      
+    
+
+
+
+
+ansatz.decompose().draw(output = 'mpl')
+
+
+# In[8]:
+
+
+print("energy= ", enrgy3)
+print("")
+print("=======================***************============================")
+print("particle number"  ,par3)
+print("")
+print("=======================***************============================")
+print("spin"  ,spin3)
+
+
+# In[1]:
+
+
+from qiskit import*
+from qiskit.circuit import Parameter
+import numpy as np
+from qiskit import Aer
+from qiskit_nature.drivers import UnitsType, Molecule
+from qiskit_nature.drivers.second_quantization import (
+    ElectronicStructureDriverType,
+    ElectronicStructureMoleculeDriver,
+)
+from qiskit_nature.problems.second_quantization import ElectronicStructureProblem
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper, ParityMapper, BravyiKitaevMapper
+
+
+enrgy6=[]
+par6=[]
+spin6=[]
+b=0.735
+for a in np.arange(0.6,0.8,0.01, dtype=object):
+    molecule = Molecule(
+        geometry=[["H", [0.0, 0.0, 0.0]], ["H", [a,0.0, 0]], ["H", [0.0, b, 0]],["H", [a, b, 0.0]]], charge=0, multiplicity=1
+    )
+    driver = ElectronicStructureMoleculeDriver(
+        molecule, basis="sto3g", driver_type=ElectronicStructureDriverType.PYQUANTE
+    )
+
+    es_problem = ElectronicStructureProblem(driver)
+    qubit_converter = QubitConverter(ParityMapper(),two_qubit_reduction=True)
+    from qiskit.providers.aer import StatevectorSimulator
+    
+    from qiskit import Aer
+    from qiskit.utils import QuantumInstance,algorithm_globals
+    from qiskit_nature.algorithms import VQEUCCFactory
+
+    quantum_instance = QuantumInstance(backend=Aer.get_backend("aer_simulator_statevector"))
+    vqe_solver = VQEUCCFactory(quantum_instance)
+
+    from qiskit.algorithms import VQE
+    from qiskit.circuit.library import TwoLocal
+
+    seed = 50
+    algorithm_globals.random_seed = seed
+    
+    num_qubits = 6
+
+    
+    layer_1 = [(0, 5),(1,4),(2,3)]
+    layer_2 = [(0, 3),(1,2),(4,5)]
+    
+    
+    ansatz=TwoLocal(num_qubits, 'ry', 'cx',[layer_1,layer_2]
+                      , reps=6,insert_barriers=True, parameter_prefix = 'theta')
+    
+  
+    
+    
+    
+    from qiskit.algorithms.optimizers import SLSQP
+    slsqp = SLSQP(maxiter=200)
+    another_solver = VQE(
+        ansatz,optimizer=slsqp,
+        quantum_instance=QuantumInstance(Aer.get_backend('statevector_simulator'), seed_transpiler=seed, seed_simulator=seed),
+    )
+    from qiskit_nature.algorithms import GroundStateEigensolver
+
+    calc = GroundStateEigensolver(qubit_converter, another_solver)
+    res = calc.solve(es_problem)
+
+    print("a:",a)
+    print("")
+    print(res)
+    print("")
+    print("Total ground state energy=",res.total_energies)
+    print("")
+    #print("absolute tolerance= ",  res.ABSOLUTE_TOLERANCE )
+    print("=======================***************============================")
+    enrgy6.append(res.total_energies[0].real)
+    par6.append(res.num_particles)
+    spin6.append(res.spin)
+    
+      
+    
+
+
+
+
+ansatz.decompose().draw(output = 'mpl')
+
+
+# In[2]:
+
+
+print("energy= ", enrgy6)
+print("")
+print("=======================***************============================")
+print("particle number"  ,par6)
+print("")
+print("=======================***************============================")
+print("spin"  ,spin6)
+
+
+# In[3]:
+
+
+from qiskit import*
+from qiskit.circuit import Parameter
+import numpy as np
+from qiskit import Aer
+from qiskit_nature.drivers import UnitsType, Molecule
+from qiskit_nature.drivers.second_quantization import (
+    ElectronicStructureDriverType,
+    ElectronicStructureMoleculeDriver,
+)
+from qiskit_nature.problems.second_quantization import ElectronicStructureProblem
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.mappers.second_quantization import JordanWignerMapper, ParityMapper, BravyiKitaevMapper
+
+
+enrgy8=[]
+par8=[]
+spin8=[]
+b=0.735
+for a in np.arange(0.6,0.8,0.01, dtype=object):
+    molecule = Molecule(
+        geometry=[["H", [0.0, 0.0, 0.0]], ["H", [a,0.0, 0]], ["H", [0.0, b, 0]],["H", [a, b, 0.0]]], charge=0, multiplicity=1
+    )
+    driver = ElectronicStructureMoleculeDriver(
+        molecule, basis="sto3g", driver_type=ElectronicStructureDriverType.PYQUANTE
+    )
+
+    es_problem = ElectronicStructureProblem(driver)
+    qubit_converter = QubitConverter(ParityMapper(),two_qubit_reduction=True)
+    from qiskit.providers.aer import StatevectorSimulator
+    
+    from qiskit import Aer
+    from qiskit.utils import QuantumInstance,algorithm_globals
+    from qiskit_nature.algorithms import VQEUCCFactory
+
+    quantum_instance = QuantumInstance(backend=Aer.get_backend("aer_simulator_statevector"))
+    vqe_solver = VQEUCCFactory(quantum_instance)
+
+    from qiskit.algorithms import VQE
+    from qiskit.circuit.library import TwoLocal
+
+    seed = 50
+    algorithm_globals.random_seed = seed
+    
+    num_qubits = 6
+
+    
+    layer_1 = [(0, 5),(1,4),(2,3)]
+    layer_2 = [(0, 3),(1,2),(4,5)]
+    
+    
+    ansatz=TwoLocal(num_qubits, 'ry', 'cx',[layer_1,layer_2]
+                      , reps=8,insert_barriers=True, parameter_prefix = 'theta')
+    
+  
+    
+    
+    
+    from qiskit.algorithms.optimizers import SLSQP
+    slsqp = SLSQP(maxiter=200)
+    another_solver = VQE(
+        ansatz,optimizer=slsqp,
+        quantum_instance=QuantumInstance(Aer.get_backend('statevector_simulator'), seed_transpiler=seed, seed_simulator=seed),
+    )
+    from qiskit_nature.algorithms import GroundStateEigensolver
+
+    calc = GroundStateEigensolver(qubit_converter, another_solver)
+    res = calc.solve(es_problem)
+
+    print("a:",a)
+    print("")
+    print(res)
+    print("")
+    print("Total ground state energy=",res.total_energies)
+    print("")
+    #print("absolute tolerance= ",  res.ABSOLUTE_TOLERANCE )
+    print("=======================***************============================")
+    enrgy8.append(res.total_energies[0].real)
+    par8.append(res.num_particles)
+    spin8.append(res.spin)
+    
+      
+    
+
+
+
+
+ansatz.decompose().draw(output = 'mpl')
+
+
+# In[4]:
+
+
+print("energy= ", enrgy8)
+print("")
+print("=======================***************============================")
+print("particle number"  ,par8)
+print("")
+print("=======================***************============================")
+print("spin"  ,spin8)
+
+
+# In[ ]:
+
+
+
+
